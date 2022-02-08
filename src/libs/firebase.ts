ca94df9fa13c5d921eba9b16c562e9a87a6aab41
Wesley Diogo <wesleydiogo@live.com>
2022-02-08 17:25:07 -0300

    v1.4

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APPKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_ADDID
+  apiKey: "AIzaSyACi60De4mwEafXg-tyXjUMJpyYNW3_I9Q",
+  authDomain: "wdreactgallery.firebaseapp.com",
+  projectId: "wdreactgallery",
+  storageBucket: "wdreactgallery.appspot.com",
+  messagingSenderId: "404373721659",
+  appId: "1:404373721659:web:346aa47f4a842fbbe3821e"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
